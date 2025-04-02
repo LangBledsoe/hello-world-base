de28dc7ff0f6d815b2115973528d6d07bd28efa2
Lang Bledsoe <lang.c.bledsoe@gmail.com>
2025-04-02 08:16:54 -0700

    Initalize hello-world-base

---
+++ src/main/java/com/example/HelloWorldBase.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class HelloWorldBase {
+    public static String getMessage() {
+        return "Hello from Base!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getMessage());
+    }
+}
